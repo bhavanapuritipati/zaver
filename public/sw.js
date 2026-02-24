668abb742ed82c2a2e3360cfc57159e98a507505
bhavanapuritipati <puritipatibhavana@gmail.com>
2026-02-24 19:19:38 +0530

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'zaver-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/assets/index-084125b5.css',
+    '/assets/index-7f5c52d7.js',
+    '/images/hero_banner.png',
+    '/images/ring_diamond_solitaire.png',
+    '/images/ring_emerald_halo.png',
+    '/images/necklace_diamond.png',
+    '/images/necklace_pearl.png',
+    '/images/earring_diamond_drop.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
